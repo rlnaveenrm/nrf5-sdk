b2fe2059dd19e38097ea4ecacb46ae7114c9ac8b
Naveenrajamaran <rlnaveenrm@gmail.com>
2017-12-17 20:30:32 +0530

    Initial commit

---
+++ nRF52-blinky-test/pca10040/s132/armgcc/_build/nrf52832_xxaa_sdk_errors.c.d
@@ -0,0 +1,35 @@
+_build/nrf52832_xxaa_sdk_errors.c.o: \
+ ../../../../nRF5_SDK_12.3.0_d7731ad/components/libraries/util/sdk_errors.c \
+ ../../../../nRF5_SDK_12.3.0_d7731ad/components/libraries/util/sdk_errors.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../nRF5_SDK_12.3.0_d7731ad/components/softdevice/s132/headers/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../nRF5_SDK_12.3.0_d7731ad/components/libraries/util/nordic_common.h
+
+../../../../nRF5_SDK_12.3.0_d7731ad/components/libraries/util/sdk_errors.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../nRF5_SDK_12.3.0_d7731ad/components/softdevice/s132/headers/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../nRF5_SDK_12.3.0_d7731ad/components/libraries/util/nordic_common.h:
